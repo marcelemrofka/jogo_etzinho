5b967fa3419433227265952d4bea0a126f559ea3
marcelemrofka <111612794+marcelemrofka@users.noreply.github.com>
2025-09-22 19:53:19 -0300

    ajusta tela inicial

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+
+    public GameObject imagem01;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BtnChangeImage(){
+        //toda vez q clicar no botao vai trocar a imagem
+        //SetActivite
+    }
+
+}
